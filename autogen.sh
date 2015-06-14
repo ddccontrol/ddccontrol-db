e27401a3b950cd532ed61aa820882c6332552d3e
Jordan Klassen <jordan@klassen.me.uk>
2015-06-14 16:06:13 -0700

    Update gitignore and workaround sed bug

--- autogen.sh
+++ autogen.sh
@@ -9,9 +9,12 @@ mv Makefile.am~ Makefile.am
 
 echo "Running intltoolize..."
 intltoolize --copy --force --automake
-sed -e "s|\@INSTOBJEXT\@|.mo|" -i po/Makefile.in.in
-sed -e "s|\@CATOBJEXT\@|.gmo|" -i po/Makefile.in.in
-sed -e "s|\@GENCAT\@|gencat|" -i po/Makefile.in.in
+
+sed() { local fname perm; fname=$(eval echo \$$#); perm="$(stat -c "%a" "$fname")"; env sed "$@"; chmod "$perm" "$fname"; }
+sed -e "s|\@INSTOBJEXT\@|.mo|" -i'~' po/Makefile.in.in
+sed -e "s|\@CATOBJEXT\@|.gmo|" -i'~' po/Makefile.in.in
+sed -e "s|\@GENCAT\@|gencat|" -i'~' po/Makefile.in.in
+unset -f sed
 
 echo "Running aclocal..."
 aclocal -I m4
